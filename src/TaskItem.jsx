951d7bc7e61810fd1f858c42e5e2d79ef03c0d0e
Jonathan <jonathan.huertas.contreras@gmail.com>
2025-11-14 18:10:00 -0500

    refactor: use the TaskItem component

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,18 +1,21 @@
-export function TaskItem({ tasks, editTask, deleteTask }) {
+export function TaskItem({ tasks, editTask, deleteTask, toggleTask }) {
   return (
     <ul>
-      {tasks.map((task, index) => (
-        <li key={index}>
-          <input
-            type="checkbox"
-            name="checkbox"
-            checked={isCompleted}
-            onChange={handleCheckBoxChange} />
-          {task}
-          <button onClick={() => editTask(index)}>Edit</button>
-          <button onClick={() => deleteTask(index)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+      {
+        tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            {console.log(task)}
+            <input
+              type="checkbox"
+              name="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(index)} />
+            {task.text}
+            <button onClick={() => editTask(index)}>Edit</button>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li >
+        ))
+      }
+    </ul >
   )
 }
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -6,18 +6,12 @@ export function TaskList({ tasks, editTask, deleteTask, toggleTask }) {
       {tasks.length === 0
         ? (<p>No hay ninguna tarea</p>)
         : (
-          tasks.map((task, index) => (
-            <li key={index} className={task.completed ? 'completed' : ''}>
-              <input
-                type="checkbox"
-                name="checkbox"
-                checked={task.completed}
-                onChange={() => toggleTask(index)} />
-              {task.text}
-              <button onClick={() => editTask(index)}>Edit</button>
-              <button onClick={() => deleteTask(index)}>Delete</button>
-            </li>
-          ))
+          <TaskItem
+            tasks={tasks}
+            editTask={editTask}
+            deleteTask={deleteTask}
+            toggleTask={toggleTask} />
+
         )}
     </div>
   )
